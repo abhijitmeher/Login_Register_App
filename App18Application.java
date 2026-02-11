f9da772539513b65afd864b97cce1e74b06abd11
abhijitmeher <abhijitmeher645@gmail.com>
2026-02-11 17:20:48 +0530

    Add files via upload

---
+++ App18Application.java
@@ -0,0 +1,15 @@
+package com.durgasoft.App018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App18Application {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(App18Application.class, args);
+		
+		
+	}
+}
